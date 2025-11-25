5361473ee59d868e6f85882971f928b8627a83f1
Roberto Neto <robertovanneto@gmail.com>
2025-11-25 16:08:35 -0300

    params

--- alice_with_cert.py
+++ alice_with_cert.py
@@ -8,7 +8,22 @@ from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 from cryptography import x509
 
-p = 0xFFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1
+p_hex = """
+FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD1
+29024E088A67CC74020BBEA63B139B22514A08798E3404DD
+EF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245
+E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7ED
+EE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3D
+C2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F
+83655D23DCA3AD961C62F356208552BB9ED529077096966D
+670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF
+"""
+
+# Remove whitespace and newlines
+p_hex_cleaned = "".join(p_hex.split())
+
+# Convert to integer
+p = int(p_hex_cleaned, 16)
 g = 2
 PORT = 5000
 
@@ -101,7 +116,8 @@ def recv_loop(sock, aes):
                     pt = aes.decrypt(nonce, ct, None).decode()
                 except Exception as e:
                     print("[Alice] decrypt error:", e)
-                    break
+                    print("> ", end="", flush=True)
+                    continue
 
                 print(f"\n[Bob]: {pt}")
                 print("> ", end="", flush=True)
